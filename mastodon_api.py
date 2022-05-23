f653f37658650c54411b716353dd01e28ae9ab00
Vancha <tjipke@dedrait.com>
2022-05-23 16:57:06 +0200

    added api entities

--- mastodon_api.py
+++ mastodon_api.py
@@ -32,6 +32,48 @@ class API_App(object):
         def __init__(id,username,acct,url,display_name,note,avatar,avatar_static,header,header_static,locked,emojis,discoverable,created_at,last_status_at,statuses_count):
             pass
 
+    class API_Activity(API_Entity):
+        pass
+
+    class API_Announcement(API_Entity):
+        pass
+
+    class API_AnnouncementReaction(API_Entity):
+        pass
+
+    class API_Attachment(API_Entity):
+        pass
+
+    class API_Card(API_Entity):
+        pass
+
+    class API_Context(API_Entity):
+        pass
+
+    class API_Conversation(API_Entity):
+        pass
+
+    class API_Emoji(API_Entity):
+        pass
+
+    class API_Error(API_Entity):
+        pass
+
+    class API_FeaturedTag(API_Entity):
+        pass
+
+    class API_Field(API_Entity):
+        pass
+
+    class API_Filter(API_Entity):
+        pass
+
+    class API_History(API_Entity):
+        pass
+
+    class API_IdentityProof(API_Entity):
+        pass
+
     class API_Instance(API_Entity):
         def __init__(self, uri):
             self.uri = uri
@@ -39,6 +81,50 @@ class API_App(object):
         def get_public_timeline(self):
             public_timeline = requests.get(self.uri+"/api/v1/timelines/public/").json()
             return public_timeline
+
+    class API_List(API_Entity):
+        pass
+
+    class API_Marker(API_Entity):
+        pass
+
+    class API_Mention(API_Entity):
+        pass
+
+    class API_Notification(API_Entity):
+        pass
+
+    class API_Poll(API_Entity):
+        pass
+
+    class API_Preferences(API_Entity):
+        pass
+
+    class API_PushSubscription(API_Entity):
+        pass
+
+    class API_Relationship(API_Entity):
+        pass
+
+    class API_Report(API_Entity):
+        pass
+
+    class API_Results(API_Entity):
+        pass
+
+    class API_ScheduledStatus(API_Entity):
+        pass
+
+    class API_Source(API_Entity):
+        pass
+
+    class API_Tag(API_Entity):
+        pass
+
+    class API_Token(API_Entity):
+        pass
+
+
 
 #only this code is relevant:
 #creating a new instance
